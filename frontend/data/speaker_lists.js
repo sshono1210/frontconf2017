9af2daae4a762487a53cc6698b55f021141ce45b
sshono <s.shosho1210@gmail.com>
2017-01-05 19:23:42 +0900

    スピーカー情報のデータをjsに仮で

---
+++ frontend/data/speaker_lists.js
@@ -0,0 +1,52 @@
+module.exports = [
+    {
+        title: "懇親会プレゼンWebGL 2.0時代の幕開け - ウェブのグラフィックスはどう変わるのか",
+        detail : "WebGL 2.0 がいよいよやってきますね。Webの世界においても求められるグラフィックスの質やレベルは日々変わっていっています。今現在の WebGL の立ち位置と、これから先の WebGL の可能性,、そして WebGL 2.0 の登場で見えてくるそのさらに先の世界について考えてみましょう。",
+        link: "http://chatbox-inc.com/page/member/shono/",
+        photo: "shono.jpg",
+        at: "株式会社 chatbox",
+        name: "庄埜 詩織",
+        profile: "大阪のWeb制作会社でプログラマーとして働いています。",
+        field: "designer"
+    },
+    {
+        title: "懇親会プレゼンWebGL 2.0時代の幕開け - ウェブのグラフィックスはどう変わるのか",
+        detail : "WebGL 2.0 がいよいよやってきますね。Webの世界においても求められるグラフィックスの質やレベルは日々変わっていっています。今現在の WebGL の立ち位置と、これから先の WebGL の可能性,、そして WebGL 2.0 の登場で見えてくるそのさらに先の世界について考えてみましょう。",
+        link: "http://chatbox-inc.com/page/member/shono/",
+        photo: "shono.jpg",
+        at: "株式会社 chatbox",
+        name: "庄埜 詩織",
+        profile: "大阪のWeb制作会社でプログラマーとして働いています。",
+        field: "markup"
+    },
+    {
+        title: "懇親会プレゼンWebGL 2.0時代の幕開け - ウェブのグラフィックスはどう変わるのか",
+        detail : "WebGL 2.0 がいよいよやってきますね。Webの世界においても求められるグラフィックスの質やレベルは日々変わっていっています。今現在の WebGL の立ち位置と、これから先の WebGL の可能性,、そして WebGL 2.0 の登場で見えてくるそのさらに先の世界について考えてみましょう。",
+        link: "http://chatbox-inc.com/page/member/shono/",
+        photo: "shono.jpg",
+        at: "株式会社 chatbox",
+        name: "庄埜 詩織",
+        profile: "大阪のWeb制作会社でプログラマーとして働いています。",
+        field: "engineer"
+    },
+    {
+        title: "懇親会プレゼンWebGL 2.0時代の幕開け - ウェブのグラフィックスはどう変わるのか",
+        detail : "WebGL 2.0 がいよいよやってきますね。Webの世界においても求められるグラフィックスの質やレベルは日々変わっていっています。今現在の WebGL の立ち位置と、これから先の WebGL の可能性,、そして WebGL 2.0 の登場で見えてくるそのさらに先の世界について考えてみましょう。",
+        link: "http://chatbox-inc.com/page/member/shono/",
+        photo: "shono.jpg",
+        at: "株式会社 chatbox",
+        name: "庄埜 詩織",
+        profile: "大阪のWeb制作会社でプログラマーとして働いています。",
+        field: "designer"
+    },
+    {
+        title: "懇親会プレゼンWebGL 2.0時代の幕開け - ウェブのグラフィックスはどう変わるのか",
+        detail : "WebGL 2.0 がいよいよやってきますね。Webの世界においても求められるグラフィックスの質やレベルは日々変わっていっています。今現在の WebGL の立ち位置と、これから先の WebGL の可能性,、そして WebGL 2.0 の登場で見えてくるそのさらに先の世界について考えてみましょう。",
+        link: "http://chatbox-inc.com/page/member/shono/",
+        photo: "shono.jpg",
+        at: "株式会社 chatbox",
+        name: "庄埜 詩織",
+        profile: "大阪のWeb制作会社でプログラマーとして働いています。",
+        field: "markup"
+    }
+];
